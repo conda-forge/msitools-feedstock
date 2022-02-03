36eb8624fb72d92a59d3c9da5faabe2c3026887a
Uwe L. Korn <xhochy@users.noreply.github.com>
2022-02-03 08:25:15 +0100

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,7 +4,7 @@ cp $BUILD_PREFIX/share/gnuconfig/config.* ./build-aux
 
 ./configure --prefix=$PREFIX
 make -j${CPU_COUNT}
-if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR}" != "" || "${target_platform}" != osx-64  ]]; then
+if [[ ("${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR}" != "") && "${target_platform}" != osx-64  ]]; then
   make check -j${CPU_COUNT}
 fi
 make install
