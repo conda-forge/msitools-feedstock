6c80034eb68d2724e43e459756915e59f109cf42
Uwe L. Korn <uwe.korn@quantco.com>
2022-12-12 09:50:16 +0100

    Push current development state

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-# Get an updated config.sub and config.guess
-cp $BUILD_PREFIX/share/gnuconfig/config.* ./build-aux
 
-./configure --prefix=$PREFIX
-make -j${CPU_COUNT}
-if [[ ("${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR}" != "") && "${target_platform}" != osx-64  ]]; then
-  make check -j${CPU_COUNT}
+set -euxo pipefail
+
+if [[ "${build_platform}" != "${target_platform}" ]]; then
+  # Generation of introspection isn't currently working in cross-compilation mode.
+  export MESON_ARGS="${MESON_ARGS:-} -Dintrospection=false"
 fi
-make install
+meson ${MESON_ARGS:-} --buildtype=release --prefix="$PREFIX" --backend=ninja -Dlibdir=lib ..
+ninja
+ninja install
