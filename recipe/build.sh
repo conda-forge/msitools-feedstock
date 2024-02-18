f90d7d1738eaa1be9757d3b9de99ef3e7b5991db
Uwe L. Korn <uwelk@xhochy.com>
2024-02-18 10:58:54 +0100

    Patch vapidir lookup

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
-# Get an updated config.sub and config.guess
-cp $BUILD_PREFIX/share/gnuconfig/config.* ./build-aux
 
-./configure --prefix=$PREFIX
-make -j${CPU_COUNT}
-if [[ ("${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR}" != "") && "${target_platform}" != osx-64  ]]; then
-  make check -j${CPU_COUNT}
+set -euxo pipefail
+
+if [[ "${target_platform}" == osx-* ]]; then
+  export CFLAGS="${CFLAGS} -Wno-incompatible-function-pointer-types"
+  # export CXXFLAGS="${CXXFLAGS} -Wno-incompatible-function-pointer-types"
 fi
-make install
+
+meson setup builddir/ \
+  ${MESON_ARGS} \
+  --buildtype=release \
+  --prefix=$PREFIX \
+  || { cat builddir/meson-logs/meson-log.txt; exit 1; }
+
+ninja -C builddir/ -j${CPU_COUNT}
+
+if [[ ("${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR}" != "") && "${target_platform}" != osx-64  ]]; then
+  ninja -C builddir/ -j${CPU_COUNT} test
+fi
+
+ninja -C builddir/ install
+
