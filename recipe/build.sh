a3cdc9f2bebb46f46998d4abdf2e1bbcd0046182
Uwe L. Korn <uwe.korn@quantco.com>
2024-02-19 16:59:21 +0100

    Add gir cross

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,12 +4,54 @@ set -euxo pipefail
 
 if [[ "${target_platform}" == osx-* ]]; then
   export CFLAGS="${CFLAGS} -Wno-incompatible-function-pointer-types"
-  # export CXXFLAGS="${CXXFLAGS} -Wno-incompatible-function-pointer-types"
+fi
+
+meson_config_args=(
+    --buildtype=release
+    --backend=ninja
+    -Dlibdir=lib
+    -Dintrospection=enabled
+)
+
+if [[ "$CONDA_BUILD_CROSS_COMPILATION" == "1" ]]; then
+  unset _CONDA_PYTHON_SYSCONFIGDATA_NAME
+  (
+    mkdir -p native-build
+
+    export CC=$CC_FOR_BUILD
+    export OBJC=$OBJC_FOR_BUILD
+    export AR=($CC_FOR_BUILD -print-prog-name=ar)
+    export NM=($CC_FOR_BUILD -print-prog-name=nm)
+    export LDFLAGS=${LDFLAGS//$PREFIX/$BUILD_PREFIX}
+    export PKG_CONFIG_PATH=${BUILD_PREFIX}/lib/pkgconfig
+
+    # Unset them as we're ok with builds that are either slow or non-portable
+    unset CFLAGS
+    unset CPPFLAGS
+    export host_alias=$build_alias
+    export PKG_CONFIG_PATH=$BUILD_PREFIX/lib/pkgconfig
+    export GIO_MODULE_DIR=$BUILD_PREFIX/lib/gio/modules
+
+    meson setup native-build \
+        "${meson_config_args[@]}" \
+        --buildtype=release \
+        --prefix=$BUILD_PREFIX \
+        -Dlibdir=lib \
+        --wrap-mode=nofallback
+
+    # This script would generate the functions.txt and dump.xml and save them
+    # This is loaded in the native build. We assume that the functions exported
+    # by glib are the same for the native and cross builds
+    export GI_CROSS_LAUNCHER=$BUILD_PREFIX/libexec/gi-cross-launcher-save.sh
+    ninja -v -C native-build -j ${CPU_COUNT}
+    ninja -C native-build install -j ${CPU_COUNT}
+  )
+  export GI_CROSS_LAUNCHER=$BUILD_PREFIX/libexec/gi-cross-launcher-load.sh
 fi
 
 meson setup builddir/ \
   ${MESON_ARGS} \
-  --buildtype=release \
+  "${meson_config_args[@]}" \
   --prefix=$PREFIX \
   || { cat builddir/meson-logs/meson-log.txt; exit 1; }
 
