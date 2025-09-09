971b183adc7b623d1e177e18d40fb252acf88c4b
h-vetinari <h.vetinari@gmx.com>
2025-09-09 23:04:43 +0200

    prefer `-Dbuildtype=release` for meson

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,8 +8,8 @@ fi
 
 # We will always need introspection to build the wixl command.
 meson_config_args=(
-    --buildtype=release
     --backend=ninja
+    -Dbuildtype=release
     -Dlibdir=lib
     -Dintrospection=true
 )
@@ -42,9 +42,9 @@ if [[ "${CONDA_BUILD_CROSS_COMPILATION:-0}" == "1" ]]; then
 
     meson setup native-build \
         "${meson_config_args[@]}" \
-        --buildtype=release \
-        --prefix=$BUILD_PREFIX \
+        -Dbuildtype=release \
         -Dlibdir=lib \
+        --prefix=$BUILD_PREFIX \
         --wrap-mode=nofallback
 
     # This script would generate the functions.txt and dump.xml and save them
